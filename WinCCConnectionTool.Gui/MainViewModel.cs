3d04b664737fd705b7273e476724d216b9713e57
Federico Barresi <federico.barresi@evopro-ag.de>
2019-07-31 08:50:29 +0200

    create initial gui and view model

---
+++ WinCCConnectionTool.Gui/MainViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using ReactiveUI;
+using System.Reactive;
+using System.Reactive.Linq;
+using System.Threading.Tasks;
+using System.Windows.Threading;
+
+namespace WinCCConnectionTool.Gui
+{
+    public class MainViewModel
+    {
+        public MainViewModel()
+        {
+            Initialize();
+        }
+
+        public void Initialize()
+        {
+            OpenProjectCommand = ReactiveCommand.CreateFromTask(OpenProject);
+
+        }
+
+        private Task OpenProject()
+        {
+            return Task.FromResult(Unit.Default);
+        }
+
+        public ReactiveCommand<Unit, Unit> OpenProjectCommand { get; set; }
+    }
+    
+}
